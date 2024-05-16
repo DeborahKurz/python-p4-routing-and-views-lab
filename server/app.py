6ef1f63ee1495067436ab6e03301a0e80db3f114
Deborah Kurz <DeborahKurz6@gmail.com>
2024-05-16 10:52:22 -0600

    /math/<num1><operation><num2> working

--- server/app.py
+++ server/app.py
@@ -13,11 +13,35 @@ def print_string(parameter):
     print(parameter)
     return f'{parameter}'
 
-@app.route('/count/<int:parameter>')
+@app.route('/count/<parameter>')
 def count(parameter):
-    range = range(parameter)
-    for num in range:
-        return f'{parameter}'
+    parameter = int(parameter)
+    numbers = ""
+    for num in range(parameter):
+        numbers += str(num)+"\n"
+    return numbers
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1, operation, num2):
+    result = None
+    num1 = int(num1)
+    num2 = int(num2)
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        pass
+    return str(result)
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
