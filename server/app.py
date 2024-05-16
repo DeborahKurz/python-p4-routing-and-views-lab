9ee092d9b2fa024bb4cd93f0f81afaf54446c0a8
Deborah Kurz <DeborahKurz6@gmail.com>
2024-05-16 10:29:37 -0600

    Index routed to base URL with /

--- server/app.py
+++ server/app.py
@@ -4,5 +4,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<parameter>')
+def print_string(parameter):
+    print(parameter)
+    return f'{parameter}'
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    range = range(parameter)
+    for num in range:
+        return f'{parameter}'
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
